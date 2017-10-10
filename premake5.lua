e2e0851ad3feb4fd2f67fc3d86ba46a1f07fb8b4
be5invis <belleve@typeof.net>
2017-10-10 14:51:55 +0800

    static link CRT on Windows builds

--- premake5.lua
+++ premake5.lua
@@ -1,19 +1,24 @@
 -- premake5.lua
 workspace "MOtToolkit"
 configurations { "Debug", "Release" }
+platforms { "x64", "x86" }
 location "build"
 
 project "glue-fonts"
     kind "ConsoleApp"
     language "C"
-    targetdir "bin/%{cfg.buildcfg}"
+    targetdir "bin/%{cfg.buildcfg}-%{cfg.platform}"
 
     files { "glue-fonts/**.h", "glue-fonts/**.c" }
+    
+    filter "system:windows"
+        flags { "StaticRuntime" }
+    filter {}
 
     filter "configurations:Debug"
-    defines { "DEBUG" }
-    symbols "On"
-
+        defines { "DEBUG" }
+        symbols "On"
     filter "configurations:Release"
-    defines { "NDEBUG" }
-    optimize "On"
+        defines { "NDEBUG" }
+        optimize "On"
+    filter {}
