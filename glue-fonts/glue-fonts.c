ad46103935edefeec6bb922e2699e0ceb957a3aa
be5invis <belleve@typeof.net>
2017-10-20 18:14:45 +0800

    glue-fonts: pad zero when locating tables

--- glue-fonts/glue-fonts.c
+++ glue-fonts/glue-fonts.c
@@ -27,14 +27,16 @@
 
 #include "md5.h"
 
-typedef struct {
+typedef struct
+{
   uint32_t tag;
   uint32_t chemSum;
   uint32_t offset;
   uint32_t length;
 } ot_table_record;
 
-typedef struct {
+typedef struct
+{
   uint32_t sfntVersion;
   uint16_t numTables;
   uint16_t searchRange;
@@ -42,50 +44,59 @@ typedef struct {
   uint16_t rangeShift;
 } ot_offset_table;
 
-typedef struct {
+typedef struct
+{
   uint32_t len;
-  uint8_t * data;
+  uint8_t *data;
 } ot_data;
 
-typedef struct {
+typedef struct
+{
   md5_byte_t digest[16];
-  uint8_t * ref;
+  uint8_t *ref;
   uint32_t length;
+  uint8_t pads;
   uint32_t offset;
 } ot_table_data;
 
-typedef struct {
-  ot_table_data * tbl;
+typedef struct
+{
+  ot_table_data *tbl;
   uint32_t count;
   uint32_t used;
   uint32_t offset;
 } ot_table_cache;
 
-static uint16_t parse_u16 (uint8_t * s)
+static uint16_t parse_u16(uint8_t *s)
 {
   return (*s << 8) | (*(s + 1));
 }
 
-static uint32_t parse_u32 (uint8_t * s)
+static uint32_t parse_u32(uint8_t *s)
 {
   return (*s << 24) | (*(s + 1) << 16) | (*(s + 2) << 8) | (*(s + 3));
 }
 
-static void write_u16 (FILE * f, uint16_t n)
+static void write_byte(FILE *f, uint8_t n)
+{
+  fputc(n, f);
+}
+
+static void write_u16(FILE *f, uint16_t n)
 {
   uint8_t b0 = ((n >> 010) & 0xff);
-  uint8_t b1 = (n          & 0xff);
+  uint8_t b1 = (n & 0xff);
 
   fputc(b0, f);
   fputc(b1, f);
 }
 
-static void write_u32 (FILE * f, uint32_t n)
+static void write_u32(FILE *f, uint32_t n)
 {
   uint8_t b0 = ((n >> 030) & 0xff);
   uint8_t b1 = ((n >> 020) & 0xff);
   uint8_t b2 = ((n >> 010) & 0xff);
-  uint8_t b3 = (n          & 0xff);
+  uint8_t b3 = (n & 0xff);
 
   fputc(b0, f);
   fputc(b1, f);
@@ -93,11 +104,11 @@ static void write_u32 (FILE * f, uint32_t n)
   fputc(b3, f);
 }
 
-static ot_data * read_opentype_font (const char * name)
+static ot_data *read_opentype_font(const char *name)
 {
-  FILE * f = fopen(name, "rb");
+  FILE *f = fopen(name, "rb");
   uint32_t len = 0;
-  ot_data * otf = calloc(1, sizeof(ot_data));
+  ot_data *otf = calloc(1, sizeof(ot_data));
   fseek(f, 0, SEEK_END);
   len = ftell(f);
   otf->len = len;
@@ -109,9 +120,9 @@ static ot_data * read_opentype_font (const char * name)
   return otf;
 }
 
-static ot_table_cache * collection_cache_init (uint32_t table_count, uint32_t offset_body)
+static ot_table_cache *collection_cache_init(uint32_t table_count, uint32_t offset_body)
 {
-  ot_table_cache * cache = calloc(1, sizeof(ot_table_cache));
+  ot_table_cache *cache = calloc(1, sizeof(ot_table_cache));
   cache->tbl = calloc(table_count, sizeof(ot_table_data));
   cache->count = table_count;
   cache->used = 0;
@@ -120,7 +131,7 @@ static ot_table_cache * collection_cache_init (uint32_t table_count, uint32_t of
   return cache;
 }
 
-static void collection_cache_fini (ot_table_cache * cache)
+static void collection_cache_fini(ot_table_cache *cache)
 {
   if (cache)
   {
@@ -130,14 +141,14 @@ static void collection_cache_fini (ot_table_cache * cache)
   }
 }
 
-static uint32_t cache_table_data (ot_table_cache * cache, uint8_t * ref, uint32_t length)
+static uint32_t cache_table_data(ot_table_cache *cache, uint8_t *ref, uint32_t length)
 {
   md5_state_t state;
   md5_byte_t digest[16];
   int32_t cached_index = -1;
 
   md5_init(&state);
-  md5_append(&state, (const md5_byte_t *) ref, length);
+  md5_append(&state, (const md5_byte_t *)ref, length);
   md5_finish(&state, digest);
 
   for (uint32_t i = 0; i < cache->used; i++)
@@ -156,13 +167,15 @@ static uint32_t cache_table_data (ot_table_cache * cache, uint8_t * ref, uint32_
     cache->tbl[cache->used].ref = ref;
     memcpy(cache->tbl[cache->used].digest, digest, 16);
     cache->tbl[cache->used].offset = cache->offset;
-    cache->offset += length;
+    uint8_t pads = length % 4 ? 4 - (length % 4) : 0;
+    cache->tbl[cache->used].pads = pads;
+    cache->offset += length + pads;
     cache->used += 1;
     return cache->tbl[cache->used - 1].offset;
   }
 }
 
-int main (int argc, char ** argv)
+int main(int argc, char **argv)
 {
   if (argc < 3)
   {
@@ -171,7 +184,7 @@ int main (int argc, char ** argv)
   else
   {
     uint32_t input_count = argc - 2;
-    ot_data ** input_buffer = calloc(input_count, sizeof(ot_data *));
+    ot_data **input_buffer = calloc(input_count, sizeof(ot_data *));
     uint32_t offset_body = 0;
     uint32_t table_count = 0;
     uint32_t table_used = 0;
@@ -181,7 +194,7 @@ int main (int argc, char ** argv)
       input_buffer[i] = read_opentype_font(argv[2 + i]);
     }
 
-    FILE * f = fopen(argv[1], "wb");
+    FILE *f = fopen(argv[1], "wb");
     fprintf(f, "ttcf");
     write_u16(f, 1);
     write_u16(f, 0);
@@ -195,7 +208,7 @@ int main (int argc, char ** argv)
       table_count += parse_u16(input_buffer[i]->data + 4);
     }
 
-    ot_table_cache * cache = collection_cache_init(table_count, offset_body);
+    ot_table_cache *cache = collection_cache_init(table_count, offset_body);
 
     for (uint32_t i = 0; i < input_count; i++)
     {
@@ -205,9 +218,9 @@ int main (int argc, char ** argv)
       {
         fwrite(input_buffer[i]->data + 12 + j * 16, sizeof(uint8_t), 8, f);
         write_u32(f,
-          cache_table_data(cache,
-            input_buffer[i]->data + parse_u32(input_buffer[i]->data + 12 + j * 16 + 8),
-            parse_u32(input_buffer[i]->data + 12 + j * 16 + 12)));
+                  cache_table_data(cache,
+                                   input_buffer[i]->data + parse_u32(input_buffer[i]->data + 12 + j * 16 + 8),
+                                   parse_u32(input_buffer[i]->data + 12 + j * 16 + 12)));
         fwrite(input_buffer[i]->data + 12 + j * 16 + 12, sizeof(uint8_t), 4, f);
       }
     }
@@ -215,17 +228,21 @@ int main (int argc, char ** argv)
     for (uint32_t i = 0; i < cache->used; i++)
     {
       fwrite(cache->tbl[i].ref, sizeof(uint8_t), cache->tbl[i].length, f);
+      for (uint32_t m = 0; m < cache->tbl[i].pads; m++)
+      {
+        write_byte(f, 0);
+      }
     }
 
     collection_cache_fini(cache);
-    
+
     for (uint32_t i = 0; i < input_count; i++)
     {
       if (input_buffer[i]->data)
         free(input_buffer[i]->data);
       free(input_buffer[i]);
     }
-    
+
     if (input_buffer)
       free(input_buffer);
 
